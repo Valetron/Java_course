508293ad671b34921d17002d63c7efc3603bd2ef
Veler Jevsky <43819843+Valetron@users.noreply.github.com>
2022-09-25 21:36:06 +0000

    The 1st task

---
+++ task1/src/task1/Line.java
@@ -0,0 +1,38 @@
+package task1;
+
+public class Line
+{
+	public void print()
+	{
+		System.out.printf("Line\'s the first dot = {%f, %f}, the second one = {%f, %f}\n", this.x1, this.y1, this.x2, this.y2);
+	}
+	
+	public double[] getFirstPoint()
+	{
+		double[] data = {this.x1, this.y1};
+		return data;
+	}
+	
+	public double[] getSecondPoint()
+	{
+		double[] data = {this.x2, this.y2};
+		return data;
+	}
+	
+	public void setFirstPont(double[] coords)
+	{
+		this.x1 = coords[0];
+		this.y1 = coords[1];
+	}
+	
+	public void setSecondPont(double[] coords)
+	{
+		this.x2 = coords[0];
+		this.y2 = coords[1];
+	}
+	
+	private double x1;
+	private double y1;
+	private double x2;
+	private double y2;
+}
